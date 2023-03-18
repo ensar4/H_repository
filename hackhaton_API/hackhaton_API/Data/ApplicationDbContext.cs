b8af0f7d86f047302e20f31cdf5ffc608827b5a0
Ensar Lizde <ensar.lizde@edu.fit.ba>
2023-03-18 10:44:26 +0100

    Home i medjutabela

--- hackhaton_API/hackhaton_API/Data/ApplicationDbContext.cs
+++ hackhaton_API/hackhaton_API/Data/ApplicationDbContext.cs
@@ -8,6 +8,11 @@ namespace hackhaton_API.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Korisnik> Korisnik { get; set; }
+        public DbSet<Home> Home { get; set; }
+        public DbSet<Korisnik_Home> Korisnik_Home { get; set; }
+
+
+
 
 
         public ApplicationDbContext(
---
+++ hackhaton_API/hackhaton_API/Models/Korisnik_Home.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
+
+namespace hackhaton_API.Models
+{
+    public class Korisnik_Home
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [ForeignKey("HomeId")]
+        public Home Home { get; set; }
+        public int HomeId { get; set; }
+
+        [ForeignKey("KorisnikId")]
+        public Korisnik Korisnik { get; set; }
+        public int KorisnikId { get; set; }
+
+    }
+}
