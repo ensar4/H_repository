13eaae9bd2ae02096af84b2715590c8a611bf353
Ensar Lizde <ensar.lizde@edu.fit.ba>
2023-03-18 11:26:59 +0100

    AddVM_0

---
+++ hackhaton_API/hackhaton_API/ViewModels/HomeAddVM.cs
@@ -0,0 +1,9 @@
+﻿namespace hackhaton_API.ViewModels
+{
+    public class HomeAddVM
+    {
+        public int id { get; set; }
+        public string naziv { get; set; }
+        public string adresa { get; set; }
+    }
+}
---
+++ hackhaton_API/hackhaton_API/ViewModels/KorisnikAddVM.cs
@@ -0,0 +1,11 @@
+﻿namespace hackhaton_API.ViewModels
+{
+    public class KorisnikAddVM
+    {
+        public int id { get; set; }
+        public string ime { get; set; }
+        public string prezime { get; set; }
+        public string brojTelefona { get; set; }
+        public string mail { get; set; }
+    }
+}
