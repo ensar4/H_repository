15e330f3237f04d935425271252fc0e7cb36b099
Ensar Lizde <ensar.lizde@edu.fit.ba>
2023-03-18 12:36:25 +0100

    Tip...

---
+++ hackhaton_API/hackhaton_API/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using hackhaton_API.Data;
+using hackhaton_API.Models;
+using hackhaton_API.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hackhaton_API.Controllers
+{
+
+    //[Authorize]
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public HomeController(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+
+        [HttpPost]
+        public Home Snimi([FromBody] HomeAddVM x)
+        {
+            Home? objekat;
+
+            if (x.id == 0)
+            {
+                objekat = new Home();
+                _dbContext.Add(objekat);
+            }
+            else
+            {
+                objekat = _dbContext.Home.Find(x.id);
+            }
+
+            objekat.Id = x.id;
+            objekat.Naziv = x.naziv;
+            objekat.Adresa = x.adresa;
+
+            _dbContext.SaveChanges();
+            return objekat;
+        }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            var data = _dbContext.Home
+                .OrderBy(s => s.Id)
+                .Select(s => new HomeGetVM
+                {
+                  adresa=s.Adresa,
+                  naziv=s.Naziv
+
+                })
+                .AsQueryable();
+            return Ok(data.Take(100).ToList());
+        }
+
+      
+
+        [HttpPost("{id}")]
+        public ActionResult Delete(int id)
+        {
+            Home? home = _dbContext.Home.Find(id);
+
+            if (home == null)
+                return BadRequest("pogresan ID");
+
+            _dbContext.Remove(home);
+
+            _dbContext.SaveChanges();
+            return Ok(home);
+        }
+    }
+}
---
+++ hackhaton_API/hackhaton_API/ViewModels/HomeGetVM.cs
@@ -0,0 +1,8 @@
+﻿namespace hackhaton_API.ViewModels
+{
+    public class HomeGetVM
+    {
+        public string naziv { get; set; }
+        public string adresa { get; set; }
+    }
+}
